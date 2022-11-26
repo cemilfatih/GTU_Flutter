5f795bff6ac3937e8ff2467c9e58c1bfc05ebf9b
cemilfatih <cemilfatihyol@gmail.com>
2022-11-27 00:46:44 +0300

    A great start for a good journey.

---
+++ gtu_flutter_project/lib/pages/homeView.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class homeView extends StatelessWidget {
+  const homeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
